a17f6c748d2ec55ac47224dc25d274fc6329d3df
miladkh7 <milad_khaleghi@live.com>
2017-06-21 18:48:18 +0430

    Creat function for GMDG save layers

--- BasicGMDH.m
+++ BasicGMDH.m
@@ -13,47 +13,22 @@ t=t';
 %hanoz bakhshe validation o test ro joda nakardam ke bayad etefagh biofte
 
 %% GMDH parametes
-numberOfAllowedNeron=[20 20 20 20 20 10 5 4 3 2];% Number max allowed neron in each layer
+PSD=[10 20 20 10 10 10 10 10 10 10 10 10 10 10 10 2];% Number max allowed neron in each layer
+maxLayers=numel(PSD); % bayad montaghel shavad
 testCof=.15;
 validateCof=.15;
-%%
+%% Modeling
 nSamples=length(x(:,1));
 nElement=length(x(1,:));
 nValition=ceil(validateCof*nSamples);
 nTest=ceil(testCof*nSamples);
 nTrain=nSamples-nValition-nTest;
+layers=cell(maxLayers,1);
 
-%% initial
-% mergedList=FindMergeIndex(length(x(1,:)))
-for j=1:length(x(1,:))
-   currentNoron(j).value=x(:,j); %#ok %only for test
-end
+GMDH(PSD,x,t)
 
-j=1;
-for n=1:numel(numberOfAllowedNeron)
-%     maxNoron=numberOfNeronInLayers(n)
-%     maxNoron=maxNoron*(maxNoron-1)/2
-
-%gozine haye entekhab ro besaz
-    mergedList=FindMergeIndex(numel(currentNoron));
-    % #Sholud bayaad tedad noronahi ke sahte misharo amade onam
-    %noron hasho besaz
-    for j=1:size(mergedList,1)
-        mm=mergedList(j,1);
-        nn=mergedList(j,2);
-        [ Norons(j).value, Norons(j).Err]=CreatNewNeron(currentNoron(mm).value,currentNoron(nn).value,t);%only for test
-    end
-
-    %moratab kon
-    [~ ,sortorder]=sort([Norons.Err]);
-    Norons=Norons(sortorder);
-
-    %ezafe hasho hazf kon
-    Norons((numberOfAllowedNeron(n)+1):end)=[];
-   
-    % bebareshon be seri bad
-    currentNoron=Norons;
-    disp('salam')
-end
- mergedList=FindMergeIndex(numel(currentNoron));
-[ Noronsss.value, Noronsss.Err]=CreatNewNeron(currentNoron(mergedList(1)).value,currentNoron(mergedList(2)).value,t)
+%% Resultes 
+plot(t) 
+grid on
+hold on
+% plot(Noronsss.value)
--- NeronError.m
+++ NeronError.m
@@ -1,8 +1,9 @@
 function [y_prime, err ]=NeronError(A,y)
-    a_star=((A')*A)^(-1)*(A')*y;
-%       a_star=pinv(A)*y;
+%     a_star=((A')*A)^(-1)*(A')*y;
+      a_star=pinv(A)*y;
 %       a_star=A\y;
     y_prime=A*a_star;
-    err=sum((y-y_prime).^2);
-
+%     err=sum((y-y_prime).^2);
+    err=mean((y-y_prime).^2);
+    err=sqrt(err);
 end
